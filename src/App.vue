9d9291c4ecba7a6eb4c60f14cde4dd9122a78dab
Twan Hopmans <twanjhopmans@hotmail.com>
2023-07-22 09:43:56 +0200

    setting up header and button component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="container">
-
+  <Header title="Task Tracker"/>
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
+    Header,
+  },
+
+  data() {
+    return {
+      tasks: []
+    }
+  },
+
+  created() {
     
   }
 }
